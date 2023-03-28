ec3289a246c899b0c9fccd50efc89448ca1e667f
aditnag <iamaditnag034@gmail.com>
2023-03-28 23:31:26 +0530

    q9 done!

---
+++ python_assignment_5 -_conditions_and_loops/10_num_opr.py
@@ -0,0 +1,8 @@
+# 10. A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years.
+# Ask user for their salary and year of service and print the net bonus amount.
+
+sal, yr = list(map(int, input("Enter sal and yr: ").strip().split()))[:2]
+if yr > 5:
+    print(f"Bonus = {1.05 * sal}")
+else:
+    print(f"Bonus = {sal}")
