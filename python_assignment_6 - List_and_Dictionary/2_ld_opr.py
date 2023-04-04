49b28bffc73e56b76e556ab5baa10d5c2eb79e95
aditnag <iamaditnag034@gmail.com>
2023-04-04 21:34:18 +0530

    q02 done!

---
+++ python_assignment_6 - List_and_Dictionary/2_ld_opr.py
@@ -0,0 +1,8 @@
+# 2. Write a Python script to add a key to a dictionary.
+# Sample Dictionary : {0: 10, 1: 20}
+# Expected Result : {0: 10, 1: 20, 2: 30}
+
+given_dict = {0: 10, 1: 20}
+print(given_dict)
+given_dict["ney_key"] = 30
+print(given_dict)
