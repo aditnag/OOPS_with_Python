36f4ac82251a3694f7142433217c058fcaa83c00
aditnag <iamaditnag034@gmail.com>
2023-04-05 01:37:07 +0530

    q08 done!

---
+++ python_assignment_6 - List_and_Dictionary/8_ld_opr.py
@@ -0,0 +1,10 @@
+# 8.  Write a Python program to sum all the items in a dictionary.
+
+dic = {'a': 100, 'b': 200, 'c': 300}
+# sum = 0
+# for i in dic.values():
+#     sum += i
+#
+# print(f" Sum = {sum}")
+
+print(sum(dic.values()))
