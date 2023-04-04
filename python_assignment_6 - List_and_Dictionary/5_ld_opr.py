1a9453562003956d530411dc2903cfacdd1cbf7a
aditnag <iamaditnag034@gmail.com>
2023-04-04 22:44:52 +0530

    q05 done!

---
+++ python_assignment_6 - List_and_Dictionary/5_ld_opr.py
@@ -0,0 +1,6 @@
+# 5. Write a Python program to iterate over dictionaries using for loops.
+
+given_dic = {"1": 10, "2": 20, "3": 30, "4": 40, "5": 50, "6": 60}
+
+for i, j in given_dic.items():
+    print(f"{i} : {j}")
