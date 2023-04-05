6e866fbb224773971b6641f28130d83ceaf20a6d
aditnag <iamaditnag034@gmail.com>
2023-04-05 21:26:21 +0530

    q12 done!

---
+++ python_assignment_6 - List_and_Dictionary/13_ld_opr.py
@@ -0,0 +1,12 @@
+# 13. Write a Python program to remove duplicates from Dictionary.
+
+original_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value1', 'key4': 'value3', 'key5': 'value2'}
+unique_dict = {}
+
+for key, val in original_dict.items():
+    if val not in unique_dict.values():
+        unique_dict[key] = val
+    else:
+        continue
+
+print(unique_dict)
