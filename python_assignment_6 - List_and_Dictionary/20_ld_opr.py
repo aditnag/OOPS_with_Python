bb7fafebbaa4c5af2ed6df46c72da445988272cd
aditnag <iamaditnag034@gmail.com>
2023-04-05 23:04:17 +0530

    q20 done!

---
+++ python_assignment_6 - List_and_Dictionary/20_ld_opr.py
@@ -0,0 +1,11 @@
+# 20. Write a Python program to extend a list without append.
+# Sample data: [10, 20, 30]
+# [40, 50, 60]
+# Expected output : [40, 50, 60, 10, 20, 30]
+
+l1 = [10, 20, 30]
+l2 = [40, 50, 60]
+
+l1 += l2
+
+print(l1)
